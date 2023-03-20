e6b48cbdf3ef210cb1914577b81af4990659d015
snoh6839 <116703016+snoh6839@users.noreply.github.com>
2023-03-20 12:01:39 +0900

    create drop index

---
+++ index.sql
@@ -0,0 +1,7 @@
+-- SHOW INDEX FROM employees;
+
+-- CREATE INDEX idx_employees_last_name ON employees(last_name);
+-- SHOW INDEX FROM employees;
+
+-- DROP INDEX idx_employees_last_name ON employees;
+-- SHOW INDEX FROM employees;
