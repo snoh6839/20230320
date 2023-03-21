d4c9a0902f7bc48a97dac5f182e4e1312b0ab68e
snoh6839 <116703016+snoh6839@users.noreply.github.com>
2023-03-21 10:05:01 +0900

    kkk

--- newdbforerd.sql
+++ newdbforerd.sql
@@ -13,37 +13,37 @@
 -- 	`product_name`	VARCHAR(100)	NOT NULL
 -- );
 -- 
--- CREATE TABLE `order_info` (
--- 	`order_no`	INT(20)	NOT NULL	,
--- 	`product_no`	INT(20)	NOT NULL	,
--- 	`customer_no`	INT(11)	NOT NULL	,
--- 	`deliver_info`	ENUM(배송전, 배송중, 배송완료)	NOT NULL
--- );
--- 
--- CREATE TABLE `customer_point` (
--- 	`customer_no`	INT(11)	NOT NULL	COMMENT 'PK, FK',
--- 	`point`	INT(10)	NOT NULL	DEFAULT 0
--- );
--- 
--- ALTER TABLE `customer_info` ADD CONSTRAINT `PK_CUSTOMER_INFO` PRIMARY KEY (
--- 	`customer_no`
--- );
--- 
--- ALTER TABLE `product_list` ADD CONSTRAINT `PK_PRODUCT_LIST` PRIMARY KEY (
--- 	`product_no`
--- );
--- 
--- ALTER TABLE `order_info` ADD CONSTRAINT `PK_ORDER_INFO` PRIMARY KEY (
--- 	`order_no`
--- );
--- 
--- ALTER TABLE `customer_point` ADD CONSTRAINT `PK_CUSTOMER_POINT` PRIMARY KEY (
--- 	`customer_no`
--- );
--- 
--- ALTER TABLE `customer_point` ADD CONSTRAINT `FK_customer_info_TO_customer_point_1` FOREIGN KEY (
--- 	`customer_no`
--- )
--- REFERENCES `customer_info` (
--- 	`customer_no`
--- );
+CREATE TABLE `order_info` (
+	`order_no`	INT(20)	NOT NULL	,
+	`product_no`	INT(20)	NOT NULL	,
+	`customer_no`	INT(11)	NOT NULL	,
+	`deliver_info`	ENUM('배송전', '배송중', '배송완료')	NOT NULL
+);
+
+CREATE TABLE `customer_point` (
+	`customer_no`	INT(11)	NOT NULL	COMMENT 'PK, FK',
+	`point`	INT(10)	NOT NULL	DEFAULT 0
+);
+
+ALTER TABLE `customer_info` ADD CONSTRAINT `PK_CUSTOMER_INFO` PRIMARY KEY (
+	`customer_no`
+);
+
+ALTER TABLE `product_list` ADD CONSTRAINT `PK_PRODUCT_LIST` PRIMARY KEY (
+	`product_no`
+);
+
+ALTER TABLE `order_info` ADD CONSTRAINT `PK_ORDER_INFO` PRIMARY KEY (
+	`order_no`
+);
+
+ALTER TABLE `customer_point` ADD CONSTRAINT `PK_CUSTOMER_POINT` PRIMARY KEY (
+	`customer_no`
+);
+
+ALTER TABLE `customer_point` ADD CONSTRAINT `FK_customer_info_TO_customer_point_1` FOREIGN KEY (
+	`customer_no`
+)
+REFERENCES `customer_info` (
+	`customer_no`
+);
